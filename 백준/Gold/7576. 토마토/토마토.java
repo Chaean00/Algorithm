c2e8a802f93e71bdee57eaeba2a070525af8fb88
Chaean00 <jtkim965@naver.com>
2025-08-11 20:57:25 +0900

    [Gold V] Title: 토마토, Time: 568 ms, Memory: 121152 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,71 +1,77 @@
-import java.io.*;
+// BOJ - 7576
 import java.util.*;
+import java.io.*;
 
 class Point {
     int x, y;
 
-    public Point(int x, int y) {
+    Point (int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
 public class Main {
-
-    static int N, M, day;
-    static int[][] tomato;
-    static Queue<Point> queue;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-
-    public static void main (String[] args) throws IOException {
+    static int[] DX = {-1, 0, 1, 0};
+    static int[] DY = {0, -1, 0, 1};
+    static Queue<Point> q = new LinkedList<>();
+    static int[][] arr;
+    static boolean[][] visit;
+    static int N, M;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        tomato = new int[N][M];
-        queue = new LinkedList<>();
+
+        arr = new int[N][M];
+        visit = new boolean[N][M];
 
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<M; j++) {
-                tomato[i][j] = Integer.parseInt(st.nextToken());
-                if (tomato[i][j] == 1) {
-                    queue.offer(new Point(i, j));
+                arr[i][j] = Integer.parseInt(st.nextToken());
+
+                if (arr[i][j] == 1) {
+                    q.offer(new Point(i, j));
+                    visit[i][j] = true;
                 }
             }
         }
 
-        System.out.println(BFS());
+        bfs();
 
-    }
-
-    public static int BFS() {
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-
-            for (int d=0; d<4; d++) {
-                int p_x = point.x + dx[d];
-                int p_y = point.y + dy[d];
-
-                if (p_x < 0 || p_x >= N || p_y < 0 || p_y >= M) continue;
-                if (tomato[p_x][p_y] == 0) {
-                    queue.offer(new Point(p_x, p_y));
-                    tomato[p_x][p_y] = tomato[point.x][point.y] + 1;
-                }
-            }
-        }
+        int answer = -1;
 
         for (int i=0; i<N; i++) {
             for (int j=0; j<M; j++) {
-                if (tomato[i][j] == 0) {
-                    return -1;
+                answer = Math.max(answer, arr[i][j]);
+                if (arr[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
                 }
-                day = Math.max(day, tomato[i][j]);
             }
         }
-        return day-1;
+
+        System.out.println(answer-1);
+
     }
 
+    public static void bfs() {
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            for (int d=0; d<4; d++) {
+                int nx = p.x + DX[d];
+                int ny = p.y + DY[d];
+
+                if (nx >= N || ny >= M || nx < 0 || ny < 0) continue;
+                if (visit[nx][ny] || arr[nx][ny] == -1) continue;
+
+                q.offer(new Point(nx, ny));
+                visit[nx][ny] = true;
+                arr[nx][ny] = arr[p.x][p.y] + 1;
+            }
+        }
+    }
 }
